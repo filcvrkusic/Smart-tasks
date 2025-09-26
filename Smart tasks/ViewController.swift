f50ca2aa5b0cb40bb4f4ba95e6f8f3d68db20746
filcvrkusic <imcvrkusic@gmail.com>
2025-09-26 23:01:23 +0200

    Initial Commit

---
+++ Smart tasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart tasks
+//
+//  Created by Filip on 26. 9. 2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
